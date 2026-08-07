fa55beda33e1f661d6c4710080a6c1ede5290beb
gani24-g <mohithgani24@gmail.com>
2026-08-07 15:54:38 +0530

    Update guess_num.py

--- small_python_projects2/guess_num.py
+++ small_python_projects2/guess_num.py
@@ -1,17 +1,25 @@
+
 import random
-num=random.randint(1, 1000)
-attempts=0
-print("Welcome to the Number Guessing Game!")
-print("I have selected a number between 1 and 1000. Try to guess it!")
 while True:
-    guss=int(input("Enter your guess: "))
-    attempts+=1
-    if guss<num:
-        print("Too low! Try again.")
-    elif guss>num:
-        print("Too high! Try again.")
-    else:
-        print(f"Congratulations! You guessed the number {num} in {attempts} attempts.")
+    num=random.randint(1, 1000)
+    attempts=0
+    print("Welcome to the Number Guessing Game!")
+    print("I have selected a number between 1 and 1000. Try to guess it!")
+    while True:
+        guss = int(input("Enter your guess: "))
+        attempts += 1
+        if guss < num:
+            print("Too low! Try again.")
+        elif guss > num:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number {num} in {attempts} attempts.")
+            break
+
+        score = 1000 - attempts * 10
+        print(f"Your current score is: {score}")
+
+    play_again = input("Play again? (y/n): ").strip().lower()
+    if play_again != 'y' and play_again != 'yes':
+        print("Thanks for playing!")
         break
-    score=1000-attempts*10
-    
